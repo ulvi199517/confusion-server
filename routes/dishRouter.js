d453ce6c95d1d6998f640b09c2f886a3c9f0ae55
ulvi_199517 <ulvi199517@gmail.com>
2021-10-05 17:27:59 -0400

    configured new routes such as LeaderRouter and PromoRouter, also added REST request for dishId, promoId and leaderId to respond to specific dish, leader or promo

---
+++ routes/dishRouter.js
@@ -0,0 +1,45 @@
+const express = require('express');
+
+const dishRouter = express.Router();
+
+dishRouter.use(express.json());
+
+dishRouter.route('/')
+.all((req,res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res) => {
+    res.end('Will send all the dishes to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the dish: ' + req.body.name + ' with details ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('Put operations not supported on /dishes');
+})
+.delete((req, res, next) => {
+    res.end('Deleting all the dishes!');
+});
+dishRouter.route('/:dishId')
+.all((req,res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res) => {
+    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+})
+.post((req, res, next) => {
+    res.end('Post operations not supported on /dishes/' + req.params.dishId);
+})
+.put((req, res, next) => {
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+    res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.delete((req, res, next) => {
+    res.end('Deleting dish:' + req.params.dishId);
+});
+module.exports = dishRouter;
---
+++ routes/promoRouter.js
@@ -0,0 +1,45 @@
+const express = require('express');
+
+const promoRouter = express.Router();
+
+promoRouter.use(express.json());
+
+promoRouter.route('/')
+.all((req,res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res) => {
+    res.end('Will send all the promotions to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the promotion: ' + req.body.name + ' with details ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('Put operations not supported on /promotions');
+})
+.delete((req, res, next) => {
+    res.end('Deleting all the promotions!');
+});
+promoRouter.route('/:promoId')
+.all((req,res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res) => {
+    res.end('Will send details of the promotion: ' + req.params.promoId + ' to you!');
+})
+.post((req, res, next) => {
+    res.end('Post operations not supported on /promotions/' + req.params.promoId);
+})
+.put((req, res, next) => {
+    res.write('Updating the promotion: ' + req.params.promoId + '\n');
+    res.end('Will update the promotion: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.delete((req, res, next) => {
+    res.end('Deleting promotion:' + req.params.promoId);
+});
+module.exports = promoRouter;
