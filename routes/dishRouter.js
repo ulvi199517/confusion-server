7ddb4f4d30ed0bfd22f504f3755d09117eaf579e
ulvi_199517 <ulvi199517@gmail.com>
2021-10-12 20:29:11 -0400

    added new libraries called mongoose to communicate with database, also mongoose-currency to deal properly with price of dishes and configured dishDetails to handle all api calls, so now dishDetails route does a full-fledged API calls

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -1,45 +1,77 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const dishRouter = express.Router();
-
+const Dishes = require('../models/dishes');
 dishRouter.use(express.json());
 
 dishRouter.route('/')
-.all((req,res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
-.get((req,res) => {
-    res.end('Will send all the dishes to you!');
+
+.get((req,res,next) => {
+    Dishes.find({})
+    .then((dishes) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dishes);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .post((req, res, next) => {
-    res.end('Will add the dish: ' + req.body.name + ' with details ' + req.body.description);
+    Dishes.create(req.body)
+    .then((dish) => {
+        console.log('Dish Created', dish);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .put((req, res, next) => {
     res.statusCode = 403;
     res.end('Put operations not supported on /dishes');
 })
 .delete((req, res, next) => {
-    res.end('Deleting all the dishes!');
+    Dishes.deleteMany({})
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 });
+
 dishRouter.route('/:dishId')
-.all((req,res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
-.get((req,res) => {
-    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+.get((req,res,next) => {
+    Dishes.findById(req.params.dishId)
+    .then((dish) => {
+        console.log('Dish Created', dish);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .post((req, res, next) => {
     res.end('Post operations not supported on /dishes/' + req.params.dishId);
 })
 .put((req, res, next) => {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
+    Dishes.findByIdAndUpdate(req.params.dishId, {
+        $set: req.body
+    }, {new: true})
+    .then((dish) => {
+        console.log('Dish Created', dish);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .delete((req, res, next) => {
-    res.end('Deleting dish:' + req.params.dishId);
+    Dishes.findByIdAndRemove(req.params.dishId)
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 });
 module.exports = dishRouter;
