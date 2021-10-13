61ded56790616870961391477f838546e38d6ad3
ulvi_199517 <ulvi199517@gmail.com>
2021-10-13 16:19:21 -0400

    now user session is handled by express-session instead of cookie-parser, also used express-file-store to store session data in our session file system

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
+//const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -26,11 +28,18 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+// app.use(cookieParser('12345-67890-09876-54321'));
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 function auth (req, res, next) {
-  console.log(req.signedCookies);
-  if(!req.signedCookies.user){
+  console.log(req.session);
+  if(!req.session.user){
     var authHeader = req.headers.authorization;
     if (!authHeader) {
         var err = new Error('You are not authenticated!');
@@ -43,7 +52,7 @@ function auth (req, res, next) {
     var user = auth[0];
     var pass = auth[1];
     if (user == 'admin' && pass == 'password') {
-      res.cookie('user', 'admin', {signed: true})
+      req.session.user = 'admin'
         next(); // authorized
     } else {
         var err = new Error('You are not authenticated!');
@@ -52,7 +61,7 @@ function auth (req, res, next) {
         next(err);
     }
   } else {
-    if(req.signedCookies.user === 'admin'){
+    if(req.session.user === 'admin'){
       next();
     } else {
       var err = new Error('You are not authenticated!');     
